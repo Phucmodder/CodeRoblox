83277a277b654a630cd5f60350a9279edc457575
Phucbright <117422821+Phucbright@users.noreply.github.com>
2025-02-02 12:44:00 +0700

    Update Tele300m.lua

--- Tele300m.lua
+++ Tele300m.lua
@@ -8,7 +8,7 @@ local ESPConfig = {
     LineThickness = 1,
     BoxThickness = 2,
     TeleportSpeed = 0.00000000001,
-    MaxTeleportDistance = 10,
+    MaxTeleportDistance = 50,
     TeleportDelay = 0
 }
 
