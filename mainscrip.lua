9a149d63c89f3c414e5def2fedaf192d2ccc54cd
Phuc Modder <117422821+Phucmodder@users.noreply.github.com>
2025-02-09 21:56:02 +0700

    Update mainscrip.lua

--- mainscrip.lua
+++ mainscrip.lua
@@ -6,8 +6,8 @@ local CoreGui = game:GetService("CoreGui")
 
 -- 🔹 **Cấu hình link GitHub**
 local KeyStorageUrl = "https://raw.githubusercontent.com/Phucmodder/CodeRoblox/refs/heads/main/Key.txt"  -- Thay USERNAME và REPO
-local Script1Url = "https://raw.githubusercontent.com/Phucmodder/CodeRoblox/refs/heads/main/Chest.lua"
-local Script2Url = "https://raw.githubusercontent.com/Phucmodder/CodeRoblox/refs/heads/main/Esp.lua"
+local Script1Url = "https://raw.githubusercontent.com/Phucmodder/CodeRoblox/refs/heads/main/Esp.lua"
+local Script2Url = "https://raw.githubusercontent.com/Phucmodder/CodeRoblox/refs/heads/main/Chest.lua"
 local ThemeImageUrl = "https://raw.githubusercontent.com/Phucmodder/CodeRoblox/refs/heads/main/15413.jpg"  -- Thay bằng link ảnh theme của bạn
 
 -- 🔹 **Lưu Key**
